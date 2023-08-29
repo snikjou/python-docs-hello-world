c6fa04a21dc1c8a6a904f5a01390070386633624
Said Nikjou <snikjou@hotmail.com>
2023-08-29 09:01:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Business Applications- Cloud Solution Architect Copilot"
+    return "Hello World!"
+
+"""q:what are the most common uses for python?"""
+"""a:web development, data science, machine learning, and system scripting"""
+
+"""create a function that takes a string and returns it backwards"""
+def reverse_string(string):
+    return string[::-1]
+
+
+"""test the function"""
+print(reverse_string("hello world"))
